03186a45c949a7d8e9d5ef4942eb91fa793c1acf
Tomasz <tomasz@tomasz>
2025-08-13 17:30:23 +0200

    tutorial ciąg dalszy

--- Zadanie_1.py
+++ Zadanie_1.py
@@ -3,7 +3,7 @@ from PyQt6.QtCore import Qt
 
 
 class _Bar(QtWidgets.QWidget):
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -12,19 +12,78 @@ class _Bar(QtWidgets.QWidget):
             QtWidgets.QSizePolicy.Policy.MinimumExpanding
         )
 
+
+        if isinstance(steps, list):
+            # list of colors.
+            self.n_steps = len(steps)
+            self.steps = steps
+
+        elif isinstance(steps, int):
+            # int number of bars, defaults to red.
+            self.n_steps = steps
+            self.steps = ['red'] * steps
+
+        else:
+            raise TypeError('steps must be a list or int')
+
+        self._bar_solid_percent = 0.8
+        self._background_color = QtGui.QColor('black')
+        self._padding = 4.0  # n-pixel gap around edge.
+
     def sizeHint(self):
         return QtCore.QSize(40,120)
 
+    clickedValue = QtCore.pyqtSignal(int)
+
+
     def paintEvent(self, e):
         painter = QtGui.QPainter(self)
+
         brush = QtGui.QBrush()
         brush.setColor(QtGui.QColor('black'))
         brush.setStyle(Qt.BrushStyle.SolidPattern)
         rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
         painter.fillRect(rect, brush)
-    
+
+        # Get current state.
+        dial = self.parent()._dial
+        vmin, vmax = dial.minimum(), dial.maximum()
+        value = dial.value()
+
+        padding = 5
+
+        # Define our canvas.
+        d_height = painter.device().height() - (padding * 2)
+        d_width = painter.device().width() - (padding * 2)
+
+        # Draw the bars.
+        step_size = d_height / 5
+        bar_height = step_size * 0.6
+        bar_spacer = step_size * 0.4 / 2
+
+        pc = (value - vmin) / (vmax - vmin)
+        n_steps_to_draw = int(pc * 5)
+        brush.setColor(QtGui.QColor('red'))
+        for n in range(n_steps_to_draw):
+            rect = QtCore.QRect(
+                padding,
+                padding + d_height - ((n+1) * step_size) + bar_spacer,
+                d_width,
+                bar_height
+            )
+            painter.fillRect(rect, brush)
+
+        painter.end()
+
+    def _trigger_refresh(self):
+        self.update()
+
 
 class PowerBar(QtWidgets.QWidget):
+    """
+    Custom Qt Widget to show a power bar and dial.
+    Demonstrating compound and custom-drawn widget.
+    """
 
     def __init__(self, steps=5, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -34,11 +93,12 @@ class PowerBar(QtWidgets.QWidget):
         layout.addWidget(self._bar)
 
         self._dial = QtWidgets.QDial()
-        layout.addWidget(self._dial)
+        self._dial.valueChanged.connect(
+           self._bar._trigger_refresh
+        )
 
+        layout.addWidget(self._dial)
         self.setLayout(layout)
-    
-    
 
 
 app = QtWidgets.QApplication([])
