84beed91b1d1ca755232eba7ca5c535c3b06d907
Tomasz <tomasz@tomasz>
2025-08-14 00:32:09 +0200

    zad 2 tutorial - cd.

--- Zadanie_2.py
+++ Zadanie_2.py
@@ -1,14 +1,18 @@
-from PyQt6.QtCore import pyqtProperty
+from PyQt6.QtCore import pyqtProperty, pyqtSignal
 
 class CustomObject(QObject):
+
+    valueChanged = pyqtSignal(int)
+
     def __init__(self):
         super().__init__()
         self._value = 0        # the default value
 
-    @pyqtProperty(int)
-    def value(self):
-        return self._value
-
+    # change the setter function to be as:
     @value.setter
     def value(self, value):
-        self._value = value
+        # here, the check is very important..
+        # to prevent unneeded signal being propagated.
+        if value != self._value:
+            self._value = value
+            self.valueChanged.emit(value)
