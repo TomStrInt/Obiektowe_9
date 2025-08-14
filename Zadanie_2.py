b87db0db8534ed449a1b55adb45eb5e0516b7de1
Tomasz <tomasz@tomasz>
2025-08-14 02:57:08 +0200

    tutorial -- cd.

--- Zadanie_2.py
+++ Zadanie_2.py
@@ -1,18 +1,17 @@
-from PyQt6.QtCore import pyqtProperty, pyqtSignal
 
-class CustomObject(QObject):
 
-    valueChanged = pyqtSignal(int)
+from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCore import QPropertyAnimation, QPoint, QEasingCurve
 
+class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self._value = 0        # the default value
-
-    # change the setter function to be as:
-    @value.setter
-    def value(self, value):
-        # here, the check is very important..
-        # to prevent unneeded signal being propagated.
-        if value != self._value:
-            self._value = value
-            self.valueChanged.emit(value)
+        self.resize(600, 600)
+        self.child = QWidget(self)
+        self.child.setStyleSheet("background-color:red;border-radius:15px;")
+        self.child.resize(100, 100)
+        self.anim = QPropertyAnimation(self.child, b"pos")
+        self.anim.setEasingCurve(QEasingCurve.Type.InOutCubic)
+        self.anim.setEndValue(QPoint(400, 400))
+        self.anim.setDuration(1500)
+        self.anim.start()
